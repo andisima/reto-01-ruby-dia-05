7cfcd052089c06dbf4b74df276c1e04ef6b3754e
Andrea Silva <andrea@DESKTOP-75HTEFS.localdomain>
2020-06-25 18:11:47 -0500

    prueba

--- board.rb
+++ board.rb
@@ -1,89 +1,67 @@
 require_relative 'cell'
-class Game
-
-  attr_reader :board
-  attr_writer :size
+class Board
+  # attr_reader :board
+  # attr_writer :size
   def initialize
-    @size = 3
-    @board = [[1,1,0],
-              [0,1,1],
-              [0,1,1]]
-  end
-
-  def read
-    temp_board = create
-    puts
-    (0..2).each do |row|
-      (0..2).each do |column|
-        cell = board[row][column]
-        status = Cell.new.alive?(cell)
-        alive_neighbours = neighbours(row,column)
-        print "[#{row}, #{column}] = #{alive_neighbours}, "
-        if status && alive_neighbours >=4
-          temp_board[row][column] = 0
-        elsif status && alive_neighbours >=2
-          temp_board[row][column] = 1
-        elsif status && alive_neighbours <=1
-          temp_board[row][column] = 0
-        end
-        temp_board[row][column] = dead_cell(status, alive_neighbours)
-        # elsif status == false && alive_neighbours == 3
-        #   temp_board[row][column] = 1
-        # end
-      end
-      puts
-    end
-    temp_board
-  end
-
-  def print_board
-    # Esto nos ayuda para hacer la matrix en cuadrito
-    board.to_a.each {|r| puts r.inspect}
-  end
-
-  def neighbours(row, column)
-    neighbours = []
-    neighbours << board[row-1][column]   if inside_board?(row-1, column)
-    neighbours << board[row-1][column+1] if inside_board?(row-1, column+1)
-    neighbours << board[row-1][column-1] if inside_board?(row-1, column-1)
-    neighbours << board[row+1][column+1] if inside_board?(row+1, column+1)
-    neighbours << board[row+1][column]   if inside_board?(row+1, column)
-    neighbours << board[row+1][column-1] if inside_board?(row+1, column-1)
-    neighbours << board[row][column-1]   if inside_board?(row, column-1)
-    neighbours << board[row][column+1]   if inside_board?(row, column+1)
-    neighbours.count(1)
-  end
-
-  def inside_board?(row, column)
-    row < size && row >= 0 && column < size && column >= 0
+    @board = [[1,0,1,0], [1,0,1,1], [1,1,1,1], [0,1,0,1]]
+    @size = 4
   end
 
   def dead_cell(status, alive_neighbours)
     status == false && alive_neighbours == 3 ? 1 : 0
   end
 
-  private
+  def evaluate(row, column)
+    cell = @board[row, column]
+    status = Cell.new.alive?(cell)
+    neighbours = neighbours(row, column)
+    status && neighbours >=4 ? status = 0 : (status && neighbours >=2 ? status = 1 : (status && neighbours >=1 ? status = 0 : (!status && neighbours == 3 ? status = 1 : (nil))))
+    @new_board
+  end
+
+  def new_board(row = 0, column = 0)
+    row == @board.size-1 && column == @board.size ? (return @new_board) : (nil)
+    @new_board[row][column] = evaluate(row, column)
+  end
+
+  def neighbours(row, column)
+    neighbours = []
+    inside_board?(row-1, column) ? (neighbours << @board [row-1] [column]) : (nil)
+    inside_board?(row-1, column+1) ? (neighbours << @board [row-1] [column+1]) : (nil)
+    inside_board?(row-1, column+1) ? (neighbours << @board [row-1] [column-1]) : (nil)
+    inside_board?(row, column-1) ? (neighbours << @board [row] [column-1]) : (nil)
+    inside_board?(row+1, column-1) ? (neighbours << @board [row+1] [column-1]) : (nil)
+    inside_board?(row+1, column) ? (neighbours << @board [row+1] [column]) : (nil)
+    inside_board?(row+1, column+1) ? (neighbours << @board [row+1] [column+1]) : (nil)
+    inside_board?(row, column+1) ? (neighbours << @board [row] [column+1]) :(nil)
+    neighbours.count(1)
+    end
+
+  def inside_board?(row, column)
+    row < @size && row >= 0 && column < @size && column >= 0
+  end
+
+  def print_board #esto nos ayuda hacer la matrix en cuadrito
+    @board.to_a.each {|r| puts r.inspect}
+  end
 
   def create
-    new_board = []
-    size.times do |row|
-      new_board << []
-      size.times { |column| new_board[row][column] = 0 }
+    @new_board = []
+    @size.times do |row|
+      @new_board << []
+      @size.times { |column| @new_board[row][column] = 0 }
     end
-    new_board
+    # return @new_board
+  end
+
+  def read
+    @new_board = create
+    @board = new_board
   end
 end
 
-# game = Game.new
-# print game.board
-# puts
-# puts
-# game.read
-# puts game.size
-# game.size = 5
-
-
-# puts
-# print game.board
-# puts game.neighbours(1, 1)
-# puts game.neighbours(0, 0)
+game = Board.new
+game.print_board
+game.read
+puts "= "*50
+game.print_board
